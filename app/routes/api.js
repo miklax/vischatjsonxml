d417db9487104d2f1ecdca3c8691775c59d40a12
mikla <mikla79@gmail.com>
2015-11-10 00:43:35 +0100

    middleware token

--- app/routes/api.js
+++ app/routes/api.js
@@ -1,4 +1,4 @@
-var bodyParser	= require('body-parser'); 
+var bodyParser	= require('body-parser');
 var jwt 		= require('jsonwebtoken');
 
 var User 		= require('../models/user');
@@ -7,9 +7,9 @@ var config 		= require('../../config');
 var secretKey = config.secret
 
 module.exports = function(app, express){
-	
+
 	var apiRouter = express.Router();
-	
+
 	//atentikacija korisnika
 	apiRouter.post('/authenticate', function(req, res) {
 
@@ -22,18 +22,18 @@ module.exports = function(app, express){
 
 	    // user not found
 	    if (!user) {
-	      res.json({ 
-	      	success: false, 
-	      	message: 'User not found.' 
+	      res.json({
+	      	success: false,
+	      	message: 'User not found.'
 	    	});
 	    } else if (user) {
 
 	      // provera lozinke
 	      var validPassword = user.comparePassword(req.body.password);
 	      if (!validPassword) {
-	        res.json({ 
-	        	success: false, 
-	        	message: 'Wrong password.' 
+	        res.json({
+	        	success: false,
+	        	message: 'Wrong password.'
 	      	});
 	      } else {
 
@@ -51,23 +51,48 @@ module.exports = function(app, express){
 	          message: 'Token assigned!',
 	          token: token
 	        });
-	      }   
+	      }
 
 	    }
 
 	  });
 	});
-	
+
 	//provera tokena
-	
-	
+	apiRouter.use(function(req, res, next){
+
+		//proveri da li token postoji u headeru ili url
+		var token = req.body.token || req.query.token || req.headers['x-access-token'];
+
+		if(token){
+			jwt.verify(token, secretKey, function(err, decoded){
+				if(err){
+					res.status(403).send({
+						success: false,
+						message: 'error (403)'
+					});
+				} else {
+					//ako je sve ok, snimi req za koriscenje u ostalim rutama
+					req.decoded = decoded;
+					next(); //produzi ka route
+				}
+			});
+		} else {
+			// ukoliko nema tokena
+				res.status(403).send({
+					success: false,
+					message: 'No token.'
+				});
+		}
+	});
+
 	//testiranje rute
-	
+
 	// /users
-	
+
 	// /users/:user_id
-	
+
 	// /me endpoint -prikaz informacija korisnika
-	
-		
+
+
 };
