5f9f33d286c8584aabfadb069e3057f4bfb7672a
mikla <mikla79@gmail.com>
2015-11-09 23:10:55 +0100

    api

--- app/routes/api.js
+++ app/routes/api.js
@@ -1,14 +1,62 @@
-var bodyParser	= require('body-parser'); //TODO: ovaj call verovatno nece trebati
+var bodyParser	= require('body-parser'); 
 var jwt 		= require('jsonwebtoken');
 
 var User 		= require('../models/user');
 var config 		= require('../../config');
+
+var secretKey = config.secret
 
 module.exports = function(app, express){
 	
 	var apiRouter = express.Router();
 	
 	//atentikacija korisnika
+	apiRouter.post('/authenticate', function(req, res) {
+
+	  // pronadji korisnika
+	  User.findOne({
+	    username: req.body.username
+	  }).select('name username password').exec(function(err, user) {
+
+	    if (err) throw err;
+
+	    // user not found
+	    if (!user) {
+	      res.json({ 
+	      	success: false, 
+	      	message: 'User not found.' 
+	    	});
+	    } else if (user) {
+
+	      // provera lozinke
+	      var validPassword = user.comparePassword(req.body.password);
+	      if (!validPassword) {
+	        res.json({ 
+	        	success: false, 
+	        	message: 'Wrong password.' 
+	      	});
+	      } else {
+
+	        // user ok, kreiraj token
+	        var token = jwt.sign({
+	        	name: user.name,
+	        	username: user.username
+	        }, secretKey, {
+	          expiresInMinutes: 1440
+	        });
+
+	        // vrati token
+	        res.json({
+	          success: true,
+	          message: 'Token assigned!',
+	          token: token
+	        });
+	      }   
+
+	    }
+
+	  });
+	});
 	
 	//provera tokena
 	
