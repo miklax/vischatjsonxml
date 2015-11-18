cb62cbfd78a4a205ee8f802262c0aa6579c83437
mikla <mikla79@gmail.com>
2015-11-18 18:55:18 +0100

    app.js minnor corrections

--- public/app/app.js
+++ public/app/app.js
@@ -1,6 +1,6 @@
-var app = angular.module('chatApp', ['ngAnimate', 'app.routes', 'serviceAuth', 'mainCtrl', 'userCtrl', 'userService'])
+angular.module('chatApp', ['ngAnimate', 'app.routes', 'serviceAuth', 'mainCtrl', 'userCtrl', 'userService'])
 
-app.config(function($httpProvider) {
+.config(function($httpProvider) {
 
 	alert('test main module');
 	// zakaci interceptor na http requests
