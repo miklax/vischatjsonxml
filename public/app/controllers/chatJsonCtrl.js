650cb712edcf86b4b84cabb06174821853e8719e
mikla <mikla79@gmail.com>
2015-11-29 06:26:36 +0100

    clean, fix, small updates

--- public/app/controllers/chatJsonCtrl.js
+++ public/app/controllers/chatJsonCtrl.js
@@ -27,26 +27,4 @@ var app = angular.module('chatJsonCtrl', [])
     $scope.$digest();
   });
 
-  // console.log('json controller loaded');
-  //
-  // jsonFact.doGetHistory()
-  // .success(function(data){
-  //   vm.historyData = data;
-  //   console.log(data);
-  //   console.log($rootScope.gUsername);
-  // });
-  //
-  // vm.postMsg = function(){
-  //
-  //   //ako je prazno polje ne trigeruje f()
-  //   // if (vm.msgData.msg !== undefined) {
-  //     jsonFact.doPostMsg($rootScope.gUsername, vm.msgData.msgLine)
-  //     .success(function(data){
-  //       vm.msgData.msgLine = '';          //ocisti formu
-  //       vm.historyData = data;    //osvezi podatke na svakom slanju poruke
-  //       vm.$digest();
-  //     });
-  //   // }
-  // };
-
 });
